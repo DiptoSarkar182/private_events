f196fe21bda09a411fb0b02bddb303e6ea32fd3a
DiptoSarkar182 <diptomanchester2018@gmail.com>
2024-03-22 14:14:56 +0600

    added css styling with tailwind

--- app/controllers/event_joinings_controller.rb
+++ app/controllers/event_joinings_controller.rb
@@ -1,8 +1,14 @@
 class EventJoiningsController < ApplicationController
   before_action :authenticate_user!
   def create
-    @event_joining = current_user.event_joinings.build(
-      event_id: params[:event_id])
+    @event = Event.find(params[:event_id])
+    @invitation = Invitation.find_by(event: @event, invitee: current_user)
+
+    if @invitation
+      @invitation.destroy
+    end
+
+    @event_joining = current_user.event_joinings.build(event: @event)
     if @event_joining.save
       redirect_to event_path(params[:event_id]), notice: 'You have successfully joined the event.'
     else
